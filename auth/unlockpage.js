22560ac84b142904581a8956998b3575fd15240e
germany <germaybasillo@gmail.com>
2024-05-18 15:20:57 +0800

    first commit

---
+++ auth/unlockpage.js
@@ -0,0 +1,7 @@
+
+
+
+if(sessionStorage.getItem("c_user_email") == null){
+    alert("You are not logged in. Please login or register to access this page.");
+    location.href = "../landing/landing.html";
+}
