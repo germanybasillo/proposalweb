bb1c664fbe2f9096716bd91a01d80d22dbc82b87
germany <germaybasillo@gmail.com>
2024-05-18 16:41:32 +0800

    first commit

--- js/login.js
+++ js/login.js
@@ -1,1 +1,43 @@
 
+let login = document.getElementById("login");
+if(login != undefined){
+    login.addEventListener("submit", e => {
+        e.preventDefault();
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("password").value;
+    
+        let email_err = document.getElementById("email-err");
+        let password_err = document.getElementById("password-err");
+    
+        email_err.innerText = "";
+        password_err.innerText = "";
+    
+        if(email == ""){
+            email_err.innerText = "Email is required!";
+        }else{
+            if(password == ""){
+                password_err.innerText = "Password is required!";
+            }else{
+                if(password.length < 8){
+                    password_err.innerText = "Your password length must be more than 8!";
+                }else{
+                    let users = JSON.parse(localStorage.getItem("users"));
+    
+                    let email_res = users.find(item => item.email == email);
+                    let password_res = email_res.password;
+    
+                    if(email_res == undefined){
+                        email_err.innerText = "Email is invalid!";
+                    }else{
+                        if(password_res != password){
+                            password_err.innerText = "Password is invalid!";
+                        }else{
+                            sessionStorage.setItem("c_user_username", username);
+                            location.href = "../page/home.html";
+                        }
+                    }
+                }
+            }
+        }
+    })
+}
--- js/logout.js
+++ js/logout.js
@@ -1,2 +1,11 @@
 
 
+let log_out = document.getElementById("log-out-btn");
+if(log_out != undefined){
+  log_out.addEventListener("click", () => {
+    if(window.confirm("Are you sure you want to logout? Click OK to proceed, or Cancel to stay logged in.")){
+      sessionStorage.removeItem("c_user_username");
+      location.href = "../landing/landing.html";
+    }
+  });
+}
