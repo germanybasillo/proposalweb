8b36ca71273a15f2ed1ffaa67f216832c62197be
germany <germanybasillo@gmail.com>
2024-05-20 15:40:17 +0800

    first commit

--- js/logout.js
+++ js/logout.js
@@ -1,9 +1,14 @@
 
 
+
+
+
 let log_out = document.getElementById("log-out-btn");
-if(log_out != undefined){
+let username = sessionStorage.getItem("c_user_username");
+
+if(log_out != undefined && username != null){
   log_out.addEventListener("click", () => {
-    if(window.confirm("Are you sure you want to logout? Click OK to proceed, or Cancel to stay logged in.")){
+    if(window.confirm(`Are you sure you want to logout, ${username}?`)){
       sessionStorage.removeItem("c_user_username");
       location.href = "../landing/landing.html";
     }
