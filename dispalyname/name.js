07daf06865ea36beaa050042c30296db33f33835
germany <germanybasillo@gmail.com>
2024-05-20 14:07:29 +0800

    first commit

---
+++ dispalyname/name.js
@@ -0,0 +1,7 @@
+
+
+
+let c_user_username = document.getElementById("c-user-username");
+if(c_user_username != undefined && sessionStorage.getItem("c_user_username") != null){
+    c_user_username.innerHTML = `<b>${sessionStorage.getItem("c_user_username")}</b>`;
+}
