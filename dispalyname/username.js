d267f6dbbfa2b6cea67de2655d58a06815d4eca4
germany <germanybasillo@gmail.com>
2024-05-20 13:20:19 +0800

    first commit

--- dispalyname/username.js
+++ dispalyname/username.js
@@ -3,5 +3,5 @@
 
 let c_user_username = document.getElementById("c-user-username");
 if(c_user_username != undefined && sessionStorage.getItem("c_user_username") != null){
-    c_user_username.innerHTML = `User: <b>${sessionStorage.getItem("c_user_username")}</b>`;
+    c_user_username.innerHTML = `Welcome <b>${sessionStorage.getItem("c_user_username")}</b>`;
 }
