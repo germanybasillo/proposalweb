1ab1261d37b7e94bfc52923d1c94e2d9c2c4fb5a
germany <germanybasillo@gmail.com>
2024-05-30 18:03:31 +0800

    first commit

--- js/login.js
+++ js/login.js
@@ -1,36 +1,34 @@
 let login = document.getElementById("login");
-if(login != undefined){
+if (login != undefined) {
     login.addEventListener("submit", e => {
         e.preventDefault();
         let email = document.getElementById("email").value;
         let password = document.getElementById("password").value;
-    
+
         let email_err = document.getElementById("email-err");
         let password_err = document.getElementById("password-err");
-    
+
         email_err.innerText = "";
         password_err.innerText = "";
-    
-        if(email == ""){
+
+        if (email == "") {
             email_err.innerText = "Email is required!";
-        }else{
-            if(password == ""){
+        } else {
+            if (password == "") {
                 password_err.innerText = "Password is required!";
-            }else{
-                if(password.length < 8){
+            } else {
+                if (password.length < 8) {
                     password_err.innerText = "Your password length must be more than 8!";
-                }else{
+                } else {
                     let users = JSON.parse(localStorage.getItem("users"));
-    
                     let email_res = users.find(item => item.email == email);
-                    let password_res = email_res.password;
-    
-                    if(email_res == undefined){
+                    if (email_res == undefined) {
                         email_err.innerText = "Email is invalid!";
-                    }else{
-                        if(password_res != password){
+                    } else {
+                        let password_res = email_res.password;
+                        if (password_res != password) {
                             password_err.innerText = "Password is invalid!";
-                        }else{
+                        } else {
                             sessionStorage.setItem("c_user_username", email_res.username);
                             location.href = "../page/home.html";
                         }
@@ -38,5 +36,5 @@ if(login != undefined){
                 }
             }
         }
-    })
+    });
 }
